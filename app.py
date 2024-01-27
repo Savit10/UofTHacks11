84bdbc8d77f3915b2675064d62f41d3edb5cc550
Steelcrawler <umailadi@gmail.com>
2024-01-27 13:30:16 -0500

    edited web app

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-from flask import Flask, render_template
-import math
+from flask import Flask
 app = Flask(__name__)
-import os
-
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Hello"
 
 if __name__ == '__main__':
     app.run(debug=True)
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
-
-#bruh moment
