f704905ad8f3ce48d6db21160aff67f2a0571cd2
anakin-vader20 <89256588+anakin-vader20@users.noreply.github.com>
2024-01-27 17:58:52 -0500

    Linking cohere-ai with Flask and HTML

--- userquestion.py
+++ userquestion.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, redirect, url_for
-
+from cohereaifile import get_response
 # Flask constructor
 app = Flask(__name__, template_folder="templates")   
 # A decorator used to tell the application
@@ -12,16 +12,31 @@ output = [{"input": "Sample input", "output": "Sample output"}]
 def index():
     return render_template("index.html", output=output)
 
-@app.route('/spongebob')
+@app.route('/spongebob', methods=["GET", "POST"])
 def spongebob():
+    if request.method == "POST":
+        input = request.form.get("user-input")
+        screenOutput = get_response(input)
+        output.append({input, screenOutput})
+        return render_template('spongebob.html', output=output)
     return render_template('spongebob.html')
 
-@app.route('/sandy')
+@app.route('/sandy', methods=["GET", "POST"])
 def sandy():
+    if request.method == "POST":
+        input = request.form.get("user-input")
+        screenOutput = get_response(input)
+        output.append({input, screenOutput})
+        return render_template('sandy.html', output=output)
     return render_template('sandy.html')
 
-@app.route('/patricks')
+@app.route('/patricks', methods=["GET", "POST"])
 def patricks():
+    if request.method == "POST":
+        input = request.form.get("user-input")
+        screenOutput = get_response(input)
+        output.append({input, screenOutput})
+        return render_template('patricks.html', output=output)
     return render_template('patricks.html')
         
 @app.route('/', methods =["GET", "POST"])
@@ -31,8 +46,6 @@ def newinputoutput():
        output.append(input, input)
        return redirect(url_for("index"))
     return render_template("spongebob.html")
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
