8ec9cd0e1fbfd01bc222e3b79740133c21bba0cc
anakin-vader20 <89256588+anakin-vader20@users.noreply.github.com>
2024-01-27 17:32:04 -0500

    html-flask implementation

--- userquestion.py
+++ userquestion.py
@@ -11,6 +11,18 @@ output = [{"input": "Sample input", "output": "Sample output"}]
 
 def index():
     return render_template("index.html", output=output)
+
+@app.route('/spongebob')
+def spongebob():
+    return render_template('spongebob.html')
+
+@app.route('/sandy')
+def sandy():
+    return render_template('sandy.html')
+
+@app.route('/patricks')
+def patricks():
+    return render_template('patricks.html')
         
 @app.route('/', methods =["GET", "POST"])
 def newinputoutput():
@@ -19,6 +31,8 @@ def newinputoutput():
        output.append(input, input)
        return redirect(url_for("index"))
     return render_template("spongebob.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
