ca12477e06d202b3dd42066a61cc85bd83c8436c
anakin-vader20 <89256588+anakin-vader20@users.noreply.github.com>
2024-01-28 00:53:54 -0500

    push

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from flask import Flask, request, render_template, redirect, url_for, session
+from flask import Flask, request, render_template, redirect, url_for
 from cohereaifile import get_response
-import atexit
-import os
 # Flask constructor
 app = Flask(__name__, template_folder="templates")  
-app.secret_key = os.urandom(24) 
 # A decorator used to tell the application
 # which URL is associated function
 
@@ -23,8 +20,8 @@ def spongebob():
     if request.method == "POST":
         input = request.form.get("user-input")
         screenOutput = get_response(input, "SpongeBob")
-        session['outputSpongebob'] = {"input": input, "screenOutput": screenOutput}
-        return render_template('spongebob.html', output=session['outputSpongebob'])
+        outputSpongebob.append({"input": input, "screenOutput": screenOutput})
+        return render_template('spongebob.html', output=outputSpongebob)
     return render_template('spongebob.html')
 
 @app.route('/sandy', methods=["GET", "POST"])
@@ -32,8 +29,8 @@ def sandy():
     if request.method == "POST":
         input = request.form.get("user-input")
         screenOutput = get_response(input, "Sandy")
-        session['outputSandy'] = {"input": input, "screenOutput": screenOutput}
-        return render_template('sandy.html', output=session['outputSandy'])
+        outputSandy.append({"input": input, "screenOutput": screenOutput})
+        return render_template('sandy.html', output=outputSandy)
     return render_template('sandy.html')
 
 @app.route('/patricks', methods=["GET", "POST"])
@@ -41,18 +38,10 @@ def patricks():
     if request.method == "POST":
         input = request.form.get("user-input")
         screenOutput = get_response(input, "Patrick")
-        session['outputPatrick'] = {"input": input, "screenOutput": screenOutput}
-        return render_template('patricks.html', output=session['outputPatrick'])
+        outputPatrick.append({"input": input, "screenOutput": screenOutput})
+        return render_template('patricks.html', output=outputPatrick)
     return render_template('patricks.html')
 
 if __name__ == '__main__':
-    def clear_lists():
-        outputSpongebob.clear()
-        outputSandy.clear()
-        outputPatrick.clear()
-        output.clear()
-
-    atexit.register(clear_lists) 
-
     app.run(debug=True)
 
