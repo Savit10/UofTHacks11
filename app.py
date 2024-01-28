8f55570acee14ebdea56a00e458362afd3fe60da
anakin-vader20 <89256588+anakin-vader20@users.noreply.github.com>
2024-01-27 19:40:46 -0500

    userquestion->app

--- app.py
+++ app.py
@@ -1,9 +1,51 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, request, render_template, redirect, url_for
+from cohereaifile import get_response
+# Flask constructor
+app = Flask(__name__, template_folder="templates")   
+# A decorator used to tell the application
+# which URL is associated function
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-#hello 
+output = [{"input": "Sample input", "output": "Sample output"}]
+
+@app.route("/")
+
+def index():
+    return render_template("index.html", output=output)
+
+@app.route('/spongebob', methods=["GET", "POST"])
+def spongebob():
+    if request.method == "POST":
+        input = request.form.get("user-input")
+        screenOutput = get_response(input, "SpongeBob")
+        output.append({"input": input, "screenOutput": screenOutput})
+        return render_template('spongebob.html', output=output)
+    return render_template('spongebob.html')
+
+@app.route('/sandy', methods=["GET", "POST"])
+def sandy():
+    if request.method == "POST":
+        input = request.form.get("user-input")
+        screenOutput = get_response(input, "Sandy")
+        output.append({"input": input, "screenOutput": screenOutput})
+        return render_template('sandy.html', output=output)
+    return render_template('sandy.html')
+
+@app.route('/patricks', methods=["GET", "POST"])
+def patricks():
+    if request.method == "POST":
+        input = request.form.get("user-input")
+        screenOutput = get_response(input, "Patrick")
+        output.append({"input": input, "screenOutput": screenOutput})
+        return render_template('patricks.html', output=output)
+    return render_template('patricks.html')
+        
+@app.route('/', methods =["GET", "POST"])
+def newinputoutput():
+    if request.method == "POST":
+       input = request.form.get("user-input") 
+       output.append(input, input)
+       return redirect(url_for("index"))
+    return render_template("spongebob.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
