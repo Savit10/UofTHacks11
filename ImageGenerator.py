4d3f5c40f2d7521c9795a94a3c1c7fa979210f2a
Steelcrawler <umailadi@gmail.com>
2024-01-28 00:25:32 -0500

    changed imagegenerator to use openai

--- ImageGenerator.py
+++ ImageGenerator.py
@@ -1,38 +1,14 @@
-import requests
-import time
-import json 
+from openai import OpenAI
 
-url = "https://api.prodia.com/v1/sd/generate"
-
-payload = { "prompt": "spongebob cooking krabbie patties" }
-headers = {
-    "accept": "application/json",
-    "content-type": "application/json",
-    "X-Prodia-Key": "8805b5b8-dab9-433e-b423-e74253cbc88b"
-}
-headers2 = {
-    "accept": "application/json",
-    "X-Prodia-Key": "8805b5b8-dab9-433e-b423-e74253cbc88b"
-}
-
-
-response = requests.post(url, json=payload, headers=headers)
-response_string = response.text
-response_dict = json.loads(response_string)
-job_id = response_dict["job"]
-
-response = requests.get("https://api.prodia.com/v1/job/" + job_id, headers=headers2)
-response_string = response.text
-response_dict = json.loads(response_string)
-status = response_dict["status"]
-while status != 'succeeded':
-    time.sleep(1)
-    response = requests.get("https://api.prodia.com/v1/job/" + job_id, headers=headers2)
-    response_string = response.text
-    response_dict = json.loads(response_string)
-    status = response_dict["status"]
-
-response_string = response.text
-response_dict = json.loads(response_string)
-    
-print(response_dict["imageUrl"])
+def generate_image(prompt) -> str:
+    my_api_key = "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"
+    client = OpenAI(api_key=my_api_key)
+    response = client.images.generate(
+        model="dall-e-2",
+        prompt=prompt,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+    image_url = response.data[0].url
+    return image_url
